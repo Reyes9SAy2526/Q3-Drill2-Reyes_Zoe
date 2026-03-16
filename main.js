34fb1a001f2c8c66456a42f6001560ffdfc6e2ff
Reyes9SAy2526 <reyes.zs@obmontessori.edu.ph>
2026-03-16 21:40:01 +0800

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-function calculateCircumference(radius) {
-  return Math.PI * (radius + radius);
+function compute_area() {
+    let base = Number(document.getElementById('base').value);
+    let height = Number(document.getElementById('height').value);
+    let area = (base * height) / 2;
+
+    document.getElementById('output').value = 'The area of the triangle is ' + area;
 }
-
-calculateCircumference(1); // 6.283185307179586
-
